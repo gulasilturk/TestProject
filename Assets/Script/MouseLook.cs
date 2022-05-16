88b4b1307ee12075c51375059d78ae53be673a43
gulasilturk <gul.asilturk@pigasoft.com>
2022-05-16 16:23:34 +0300

    camera rotation speed
    camera rotation speed has been made adjustable from the inspector.

--- Assets/Script/MouseLook.cs
+++ Assets/Script/MouseLook.cs
@@ -7,6 +7,7 @@ public class MouseLook : MonoBehaviour
     //[Range(50,500)]
     public float sens;
     public Transform body;
+    public float speed=0f;
 
     float xRot = 0f;
     // Start is called before the first frame update
@@ -18,13 +19,15 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        float rotX = Input.GetAxisRaw("Mouse X") * sens * Time.deltaTime;
-        float rotY = Input.GetAxisRaw("Mouse Y") * sens * Time.deltaTime;
+        float rotX = Input.GetAxisRaw("Mouse X") * sens * Time.deltaTime*speed;
+        float rotY = Input.GetAxisRaw("Mouse Y") * sens * Time.deltaTime*speed;
 
         xRot -= rotY;
         xRot = Mathf.Clamp(xRot, -40f, 40f);
         transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
         body.Rotate(Vector3.up * rotX);
+
+       
 
 
 
